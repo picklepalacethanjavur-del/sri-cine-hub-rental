a1b11afe31e2a59da47b6d78f5577fd2191f8c6e
picklepalacethanjavur-del <picklepalacethanjavur@gmail.com>
2026-09-20 22:04:08 -0400

    Show actual product photos for all eight cameras
    * Add model-specific product photos and source references for six cameras
    
    * Show uncropped camera product photos with neutral fallback in cards and details
    
    * Add confirmed ALEXA 35 and V-RAPTOR 8K VV product photos

--- catalog-data.js
+++ catalog-data.js
@@ -3,14 +3,14 @@ window.SRI_CATALOG = [
   {
     category: "Cameras",
     items: [
-      { n: "ARRI S35", d: "Cinema camera package." },
-      { n: "RED Raptor 8K", d: "Cinema camera package." },
-      { n: "RED Helium (DCSM2) 8K", d: "Cinema camera package." },
-      { n: "RED Gemini 5K", d: "Cinema camera package." },
-      { n: "RED Komodo 6K", d: "Cinema camera package." },
-      { n: "Sony FX3 4K", d: "Cinema camera package." },
-      { n: "Canon R5", d: "Cinema camera package." },
-      { n: "Sony A7S III", d: "Cinema camera package." }
+      { n: "ARRI S35", d: "ARRI ALEXA 35 cinema camera package.", image: "https://elitecam.com.br/cdn/shop/files/arri_k0_0042900_alexa_35_body_1654048563_1708577_800x.jpg?v=1706284162", imageSource: "https://elitecam.com.br/products/camera-arri-alexa-35" },
+      { n: "RED Raptor 8K", d: "RED V-RAPTOR 8K VV cinema camera package.", image: "https://hotrodcameras.com/cdn/shop/products/redRaptor_10__64146_500x500.jpg?v=1684605235", imageSource: "https://hotrodcameras.com/products/red-digital-cinema-v-raptor-8k-vv-dsmc3-cinema-camera-canon-rf" },
+      { n: "RED Helium (DCSM2) 8K", d: "Cinema camera package.", image: "https://www.bookmylens.com/admin/uploads/product_images/1568363861.jpg", imageSource: "https://www.bookmylens.com/product?id=298" },
+      { n: "RED Gemini 5K", d: "Cinema camera package.", image: "https://cameraambassador.com/cdn/shop/products/Red_Gemini_fa43ebe6-5a51-4584-be8c-9193152d53b3_2400x.jpg?v=1652224689", imageSource: "https://cameraambassador.com/products/red-gemini-dsmc2-5k-kit" },
+      { n: "RED Komodo 6K", d: "Cinema camera package.", image: "https://media.vocas.nl/media/catalog/product/r/e/red_komodo_6k_710-0333_front_2_1.png", imageSource: "https://vocas.nl/red-komodo-6k/" },
+      { n: "Sony FX3 4K", d: "Cinema camera package.", image: "https://cdn.rubbermonkey.com/ProductImage/Huge/312617.jpg/sony-fx3-full-frame-cinema-camera.jpg", imageSource: "https://www.rubbermonkey.com.au/Video/Cameras/Sony-FX3-Full-Frame-Cinema-Camera" },
+      { n: "Canon R5", d: "Cinema camera package.", image: "https://in.canon/media/image/2020/07/04/862a1f43feed4fab85897aee45b6324c_R5_Front_BODY.png", imageSource: "https://in.canon/en/consumer/eos-r5/body/product" },
+      { n: "Sony A7S III", d: "Cinema camera package.", image: "https://www.juzaphoto.com/shared_files/recensioni/sony_a7s_iii_s.jpg", imageSource: "https://www.juzaphoto.com/recensione.php?t=sony_a7s_iii" }
     ]
   },
   {
